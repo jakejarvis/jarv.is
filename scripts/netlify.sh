fe2d3640f99d7e3eb880c59a9b89563e198905ed
Jake Jarvis <jake@jarv.is>
2019-11-09 16:41:07 -0500

    final clean-up

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# only designed to run on Netlify's build image:
+# https://github.com/netlify/build-image
+
 set -euo pipefail
 
 BINDIR=$HOME/jj-bin
 mkdir -p $BINDIR
 
-# download custom Hugo fork release
+# download forked Hugo extended binary
 # https://github.com/jakejarvis/hugo-custom/releases
 echo "Downloading Hugo..."
 curl -sS -L https://github.com/jakejarvis/hugo-custom/releases/download/v0.53-patch3/hugo-extended -o $BINDIR/hugo-extended
@@ -21,17 +24,29 @@ echo "Downloading pngquant..."
 curl -sS -L https://github.com/imagemin/pngquant-bin/raw/master/vendor/linux/x64/pngquant -o $BINDIR/pngquant
 chmod +x $BINDIR/pngquant
 
-# make sure everything's okay
+# download optipng binary
+echo "Downloading optipng..."
+curl -sS -L https://github.com/imagemin/optipng-bin/raw/master/vendor/linux/x64/optipng -o $BINDIR/optipng
+chmod +x $BINDIR/optipng
+
+# make sure everything's OK
+echo "Is Hugo ready..?"
 $BINDIR/hugo-extended version
-#$BINDIR/jpegoptim
-#$BINDIR/pngquant
 
-# build site
-$BINDIR/hugo-extended -b "/" --gc --cleanDestinationDir --verbose
+# build Hugo site
+echo "Building site..."
+$BINDIR/hugo-extended --gc --cleanDestinationDir --verbose
 
-# optimize images
-find ./public -iname "*.jp*" -print0 | xargs -0 $BINDIR/jpegoptim --max=80 --strip-all
-find ./public -iname "*.png" -print0 | xargs -0 $BINDIR/pngquant --quality=50-70 --speed 1 --ext=.png --force
+# optimize images in publish directory
+echo "Optimizing JPEGs..."
+find ./public -iname "*.jp*" -print0 | xargs -0 $BINDIR/jpegoptim --max=80 --strip-all --quiet
+echo "Optimizing PNGs..."
+find ./public -iname "*.png" -print0 | xargs -0 $BINDIR/pngquant --quality=50-70 --speed 3 --ext=.png --force
+find ./public -iname "*.png" -print0 | xargs -0 $BINDIR/optipng -o3 -force -strip all -quiet --
 
-# remove binaries (TODO: make safe, doesn't really matter b/c docker)
+# remove binaries (kinda unsafe, but doesn't really matter b/c it's docker)
+echo "Cleaning up binaries..."
 rm -rf $BINDIR
+
+echo "All done!"
+exit 0
