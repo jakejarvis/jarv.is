2e8c5966d13c86774ebf74533f4bbf17edcc27d7
Jake Jarvis <jake@jarv.is>
2020-11-12 12:34:12 -0500

    some last-minute cleanup

--- plugins/netlify-plugin-amp-optimizer/index.js
+++ plugins/netlify-plugin-amp-optimizer/index.js
@@ -1,28 +1,31 @@
-const ampOptimizer = require('@ampproject/toolbox-optimizer').create();
-const fs = require('fs');
-const glob = require('glob');
+const ampOptimizer = require("@ampproject/toolbox-optimizer").create({
+  verbose: true,
+});
+const fs = require("fs");
+const glob = require("glob");
 
 module.exports = {
   onPostBuild: async ({ constants, utils }) => {
-    const pattern = constants.PUBLISH_DIR + '/**/amp.html';
+    // Hugo renders my AMP pages as amp.html right next to each page's index.html
+    const pattern = constants.PUBLISH_DIR + "/**/amp.html";
 
     const files = await new Promise((resolve, reject) => {
       glob(pattern, { nodir: true }, (err, files) => {
-        (err) ? reject(err) : resolve(files);
+        err ? reject(err) : resolve(files);
       });
     });
 
     await Promise.all(
       files.map(async (file) => {
-        const html = await fs.promises.readFile(file, 'utf-8');
+        const html = await fs.promises.readFile(file, "utf-8");
         const optimizedHtml = await ampOptimizer.transformHtml(html);
         await fs.promises.writeFile(file, optimizedHtml);
       })
     );
 
     utils.status.show({
-      title: 'Success! ⚡',
-      summary: `${files.length} AMP pages optimized!`
+      title: `${files.length} AMP pages optimized`,
+      summary: "Great success! ⚡",
     });
-  }
+  },
 };
