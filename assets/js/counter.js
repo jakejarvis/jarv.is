8eef3ac3cc7d14bc3e33ebbb76addb1094903429
Jake Jarvis <jake@jarv.is>
2021-05-29 20:34:32 -0400

    super simple serverless hit counter for fun

---
+++ assets/js/counter.js
@@ -0,0 +1,29 @@
+(function () {
+  // deduce a consistent identifier for this page, no matter the URL
+  var canonical = document.createElement("a");
+  canonical.href = document.querySelector("link[rel='canonical']").href;
+
+  // strip beginning and ending forward slash
+  var slug = canonical.pathname.slice(1, -1);
+
+  // this will return an error from the API anyways
+  if (!slug || slug === "/") return;
+
+  // don't continue if there isn't a span#meta-hits element on this page
+  var wrapper = document.getElementById("meta-hits");
+  if (wrapper) {
+    // eslint-disable-next-line compat/compat
+    fetch("/api/hits?slug=" + slug)
+      .then((response) => response.json())
+      .then((data) => {
+        // hide target span until the count is returned
+        wrapper.style.display = "inline";
+
+        // finally inject the hits
+        var counter = document.getElementById("hit-counter");
+        counter.appendChild(document.createTextNode(data.pretty_hits));
+        wrapper.title = data.pretty_hits + " " + data.pretty_unit;
+      })
+      .catch((error) => {});
+  }
+})();
---
+++ functions/hits.js
@@ -0,0 +1,58 @@
+const faunadb = require("faunadb");
+const numeral = require("numeral");
+const pluralize = require("pluralize");
+require("dotenv").config();
+const q = faunadb.query;
+
+exports.handler = async (event, context) => {
+  const { slug } = event.queryStringParameters;
+  const index = "hits_by_slug";
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET,
+  });
+
+  // some rudimentary error handling
+  if (!slug) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Page slug not provided.",
+      }),
+    };
+  }
+
+  // check if a counter for the slug already exists
+  const doesPageExist = await client.query(q.Exists(q.Match(q.Index(index), slug)));
+  if (!doesPageExist) {
+    await client.query(
+      q.Create(q.Collection("hits"), {
+        data: {
+          slug: slug,
+          hits: 0,
+        },
+      })
+    );
+  }
+
+  // fetch the slug's document and send it back with one more hit
+  const doc = await client.query(q.Get(q.Match(q.Index(index), slug)));
+  const new_hits = doc.data.hits + 1;
+  await client.query(
+    q.Update(doc.ref, {
+      data: {
+        hits: new_hits,
+      },
+    })
+  );
+
+  // send client the new hit count
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      slug: slug,
+      hits: new_hits,
+      pretty_hits: numeral(new_hits).format("0,0"),
+      pretty_unit: pluralize("hit", new_hits),
+    }),
+  };
+};
