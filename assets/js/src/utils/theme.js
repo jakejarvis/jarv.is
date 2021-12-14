6da7afc6725fab3ed227db394569675a056c23e3
Jake Jarvis <jake@jarv.is>
2021-12-14 18:29:28 -0500

    Update theme.js

--- assets/js/src/utils/theme.js
+++ assets/js/src/utils/theme.js
@@ -8,10 +8,10 @@ export const isDark = () => document.body.classList?.contains("dark");
 // sets appropriate `<body class="...">`
 export const setDarkClass = (dark) => {
   if (dark) {
-    document.body.classList.remove("light");
     document.body.classList.add("dark");
+    document.body.classList.remove("light");
   } else {
-    document.body.classList.remove("dark");
     document.body.classList.add("light");
+    document.body.classList.remove("dark");
   }
 };
