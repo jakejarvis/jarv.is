373ec733ea7aaafc748979b62718f724e4409d8a
Jake Jarvis <jake@jarv.is>
2021-12-29 20:21:41 -0500

    update vercel.app URLs

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteUrl:
     process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-      ? "https://jarv-is-next.vercel.app"
+      ? "https://jarv.is"
       : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
   generateRobotsTxt: true,
   sitemapSize: 99,
