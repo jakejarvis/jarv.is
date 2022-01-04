08bdd038f15fb62bdacbcad3bf7d149489f738fb
Jake Jarvis <jake@jarv.is>
2022-01-03 20:48:57 -0500

    update priorities and changefreqs

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,3 +1,5 @@
+// WARNING: THIS FILE CONTAINS HISTORICAL LEVELS OF HACKINESS AND SHOULD NOT BE REPLICATED NOR ADMIRED.
+
 import { getAllNotes } from "../lib/parse-notes";
 import { baseUrl } from "../lib/config";
 import type { GetServerSideProps } from "next";
@@ -14,33 +16,43 @@ type Page = {
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // TODO: make this not manual (serverless functions can't see /pages at runtime)
   const pages: Page[] = [
-    { relUrl: "/", priority: 1.0 }, // homepage
+    { relUrl: "/", priority: 1.0, changeFreq: "weekly" }, // homepage
     { relUrl: "/notes/" },
     { relUrl: "/birthday/" },
     { relUrl: "/cli/" },
     { relUrl: "/contact/" },
     { relUrl: "/hillary/" },
     { relUrl: "/leo/" },
-    { relUrl: "/license/", priority: 0.1 },
+    { relUrl: "/license/", priority: 0.1, changeFreq: "yearly" },
     { relUrl: "/previously/" },
-    { relUrl: "/privacy/", priority: 0.1 },
+    { relUrl: "/privacy/", priority: 0.1, changeFreq: "yearly" },
     { relUrl: "/projects/", changeFreq: "daily" },
     { relUrl: "/uses/" },
   ];
-  getAllNotes().map((note) => pages.push({ relUrl: `/notes/${note.slug}/`, lastMod: note.date }));
+
+  // push notes separately and use their metadata
+  getAllNotes().map((note) =>
+    pages.push({
+      relUrl: `/notes/${note.slug}/`,
+      // pull lastMod from front matter date
+      lastMod: note.date,
+    })
+  );
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${pages
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${pages
     .map(
       (page) => `
   <url>
     <loc>${baseUrl}${page.relUrl}</loc>
-    <priority>${page.priority || 0.7}</priority>
+    <priority>${page.priority ? page.priority.toFixed(1) : 0.7}</priority>
     <changefreq>${page.changeFreq || "monthly"}</changefreq>
     <lastmod>${page.lastMod || new Date().toISOString()}</lastmod>
   </url>`
     )
-    .join("")}
+    .join("")
+    .trim()}
 </urlset>`;
 
   const { res } = context;
