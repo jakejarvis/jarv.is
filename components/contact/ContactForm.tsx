f2e372c533a8eb8c903c7ead1a595cf7be47d217
Jake Jarvis <jake@jarv.is>
2022-01-06 11:25:50 -0500

    formik-powered contact page

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -1,40 +1,38 @@
 import { useState } from "react";
 import { useTheme } from "next-themes";
+import { Formik, Form, Field } from "formik";
 import HCaptcha from "@hcaptcha/react-hcaptcha";
+import isEmailLike from "is-email-like";
+
 import { CheckOcticon, XOcticon } from "../icons/octicons";
 import { SendIcon } from "../icons";
 
+import type { FormikHelpers } from "formik";
+
 import styles from "./ContactForm.module.scss";
+
+interface Values {
+  name: string;
+  email: string;
+  message: string;
+  "h-captcha-response": string;
+}
 
 const ContactForm = () => {
   const { resolvedTheme } = useTheme();
   // status/feedback:
   const [status, setStatus] = useState({ success: false, message: "" });
-  // keep track of fetch:
-  const [sending, setSending] = useState(false);
 
-  const onSubmit = (e) => {
-    // immediately prevent browser from actually navigating to a new page
-    e.preventDefault();
-
-    // begin the process
-    setSending(true);
-
+  const handleSubmit = (values: Values, { setSubmitting }: FormikHelpers<Values>) => {
     // extract data from form fields
     const formData = {
-      name: e.target.elements.name?.value,
-      email: e.target.elements.email?.value,
-      message: e.target.elements.message?.value,
-      "h-captcha-response": e.target.elements["h-captcha-response"]?.value,
+      name: values.name,
+      email: values.email,
+      message: values.message,
+      "h-captcha-response": values["h-captcha-response"],
     };
 
-    // some client-side validation to save requests (these are also checked on the server to be safe)
-    if (!(formData.name && formData.email && formData.message && formData["h-captcha-response"])) {
-      setSending(false);
-      setStatus({ success: false, message: "Please make sure that all fields are filled in." });
-
-      return;
-    }
+    console.log(formData);
 
     // if we've gotten here then all data is (or should be) valid and ready to post to API
     fetch("/api/contact/", {
@@ -47,8 +45,6 @@ const ContactForm = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        setSending(false);
-
         if (data.success === true) {
           // handle successful submission
           // disable submissions, hide the send button, and let user know we were successful
@@ -59,86 +55,178 @@ const ContactForm = () => {
         }
       })
       .catch((error) => {
-        const message = error instanceof Error ? error.message : "UNKNOWN_EXCEPTION";
-
-        setSending(false);
-
-        // give user feedback based on the error message returned
-        if (message === "USER_INVALID_CAPTCHA") {
-          setStatus({
-            success: false,
-            message: "Did you complete the CAPTCHA? (If you're human, that is...)",
-          });
-        } else if (message === "USER_MISSING_DATA") {
-          setStatus({
-            success: false,
-            message: "Please make sure that all fields are filled in.",
-          });
-        } else {
-          // something else went wrong, and it's probably my fault...
-          setStatus({ success: false, message: "Internal server error. Try again later?" });
-        }
-      });
+        // something else went wrong, and it's probably my fault...
+        setStatus({ success: false, message: error.message });
+      })
+      .finally(() => setSubmitting(false));
   };
 
   return (
-    <form className={styles.form} onSubmit={onSubmit} action="/api/contact/" method="POST">
-      <input type="text" name="name" placeholder="Name" required disabled={status.success} />
-      <input type="email" name="email" placeholder="Email" required disabled={status.success} />
-      <textarea name="message" placeholder="Write something..." required disabled={status.success} />
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={{
+        name: "",
+        email: "",
+        message: "",
+        "h-captcha-response": "",
+      }}
+      validate={(values) => {
+        const errors: any = {};
 
-      <div className={styles.markdown_tip}>
-        Basic{" "}
-        <a
-          href="https://commonmark.org/help/"
-          title="Markdown reference sheet"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Markdown syntax
-        </a>{" "}
-        is allowed here, e.g.: <strong>**bold**</strong>, <em>_italics_</em>, [
-        <a href="https://jarv.is" target="_blank" rel="noopener noreferrer">
-          links
-        </a>
-        ](https://jarv.is), and <code>`code`</code>.
-      </div>
+        errors.name = !values.name;
+        errors.email = !values.email || !isEmailLike(values.email);
+        errors.message = !values.message;
+        errors["h-captcha-response"] = !values["h-captcha-response"];
 
-      <div className={styles.captcha}>
-        <HCaptcha
-          sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY}
-          size="normal"
-          theme={resolvedTheme === "dark" ? "dark" : "light"}
-          onVerify={() => true} // this is allegedly optional but a function undefined error is thrown without it
-        />
-      </div>
+        if (!errors.name && !errors.email && !errors.message && !errors["h-captcha-response"]) {
+          return false;
+        }
+        /* else {
+          setStatus({ success: false, message: "missing somethign!!!" });
+        } */
 
-      <div className={styles.action_row}>
-        <button
-          className={styles.btn_submit}
-          title="Send Message"
-          aria-label="Send Message"
-          disabled={sending}
-          style={{ display: status.success ? "none" : null }}
-        >
-          {sending ? (
-            <span>Sending...</span>
-          ) : (
-            <>
-              <SendIcon className={styles.send_icon} /> <span>Send</span>
-            </>
-          )}
-        </button>
+        return errors;
+      }}
+    >
+      {({ setFieldValue, isSubmitting, touched, errors }) => (
+        <Form className={styles.form} name="contact">
+          <Field
+            type="text"
+            name="name"
+            placeholder="Name"
+            className={errors.name && touched.name ? styles.missing : undefined}
+          />
+          <Field
+            type="text"
+            name="email"
+            placeholder="Email"
+            className={errors.email && touched.email ? styles.missing : undefined}
+          />
+          <Field
+            name="message"
+            component="textarea"
+            placeholder="Write something..."
+            className={errors.message && touched.message ? styles.missing : undefined}
+          />
 
-        <span
-          className={status.success ? styles.result_success : styles.result_error}
-          style={{ display: !status.message || sending ? "none" : null }}
-        >
-          {status.success ? <CheckOcticon fill="CurrentColor" /> : <XOcticon fill="CurrentColor" />} {status.message}
-        </span>
-      </div>
-    </form>
+          <div className={styles.markdown_tip}>
+            Basic{" "}
+            <a
+              href="https://commonmark.org/help/"
+              title="Markdown reference sheet"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Markdown syntax
+            </a>{" "}
+            is allowed here, e.g.: <strong>**bold**</strong>, <em>_italics_</em>, [
+            <a href="https://jarv.is" target="_blank" rel="noopener noreferrer">
+              links
+            </a>
+            ](https://jarv.is), and <code>`code`</code>.
+          </div>
+
+          <div className={styles.captcha}>
+            <HCaptcha
+              sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY}
+              size="normal"
+              theme={resolvedTheme === "dark" ? "dark" : "light"}
+              onVerify={(token) => setFieldValue("h-captcha-response", token)}
+            />
+          </div>
+
+          <div className={styles.action_row}>
+            <button
+              className={`${styles.btn_submit} ${
+                errors["h-captcha-response"] && touched["h-captcha-response"] ? styles.missing : undefined
+              }`}
+              type="submit"
+              title="Send Message"
+              aria-label="Send Message"
+              disabled={isSubmitting}
+              style={{ display: status.success ? "none" : null }}
+            >
+              {isSubmitting ? (
+                <span>Sending...</span>
+              ) : (
+                <>
+                  <SendIcon className={styles.send_icon} /> <span>Send</span>
+                </>
+              )}
+            </button>
+
+            <span
+              className={status.success ? styles.result_success : styles.result_error}
+              style={{ display: !status.message || isSubmitting ? "none" : null }}
+            >
+              {status.success ? <CheckOcticon fill="CurrentColor" /> : <XOcticon fill="CurrentColor" />}{" "}
+              {status.message}
+            </span>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
 export default ContactForm;
+
+/*
+
+      <form className={styles.form} onSubmit={handleSubmit} action="/api/contact/" method="POST">
+        <input type="text" name="name" placeholder="Name" required disabled={status.success} />
+        <input type="email" name="email" placeholder="Email" required disabled={status.success} />
+        <textarea name="message" placeholder="Write something..." required disabled={status.success} />
+
+        <div className={styles.markdown_tip}>
+          Basic{" "}
+          <a
+            href="https://commonmark.org/help/"
+            title="Markdown reference sheet"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Markdown syntax
+          </a>{" "}
+          is allowed here, e.g.: <strong>**bold**</strong>, <em>_italics_</em>, [
+          <a href="https://jarv.is" target="_blank" rel="noopener noreferrer">
+            links
+          </a>
+          ](https://jarv.is), and <code>`code`</code>.
+        </div>
+
+        <div className={styles.captcha}>
+          <HCaptcha
+            sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY}
+            size="normal"
+            theme={resolvedTheme === "dark" ? "dark" : "light"}
+            onVerify={() => true} // this is allegedly optional but a function undefined error is thrown without it
+          />
+        </div>
+
+        <div className={styles.action_row}>
+          <button
+            className={styles.btn_submit}
+            title="Send Message"
+            aria-label="Send Message"
+            disabled={sending}
+            style={{ display: status.success ? "none" : null }}
+          >
+            {sending ? (
+              <span>Sending...</span>
+            ) : (
+              <>
+                <SendIcon className={styles.send_icon} /> <span>Send</span>
+              </>
+            )}
+          </button>
+
+          <span
+            className={status.success ? styles.result_success : styles.result_error}
+            style={{ display: !status.message || sending ? "none" : null }}
+          >
+            {status.success ? <CheckOcticon fill="CurrentColor" /> : <XOcticon fill="CurrentColor" />} {status.message}
+          </span>
+        </div>
+      </form>
+      */
