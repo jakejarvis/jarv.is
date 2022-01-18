2b7446f15e0e7a061940b0fdda797320d0de05bc
Jake Jarvis <jake@jarv.is>
2022-01-17 22:32:13 -0500

    next-mdx-remote

--- lib/mdx-components.ts
+++ lib/mdx-components.ts
@@ -1,13 +1,16 @@
 import dynamic from "next/dynamic";
 
 // Bundle these components by default:
-export { default as Image } from "../components/Image/Image";
-export { default as Figure } from "../components/Figure/Figure";
+// export { default as Image } from "../components/Image/Image";
+// export { default as Figure } from "../components/Figure/Figure";
 // `code` is intentionally lowercase here -- replaces `<code>` tag from remark
-export { default as code } from "../components/CodeBlock/CodeBlock";
+// export { default as code } from "../components/CodeBlock/CodeBlock";
 
 // All of these components are technically passed into all posts, but next/dynamic ensures they're loaded only
 // when they're referenced in the individual mdx files.
+export const Image = dynamic(() => import("../components/Image/Image"));
+export const Figure = dynamic(() => import("../components/Figure/Figure"));
+export const code = dynamic(() => import("../components/CodeBlock/CodeBlock"));
 export const Video = dynamic(() => import("../components/Video/Video"));
 export const Tweet = dynamic(() => import("../components/TweetEmbed/TweetEmbed"));
 export const Gist = dynamic(() => import("../components/GistEmbed/GistEmbed"));
