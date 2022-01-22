7b01f01c9dc5416cbe718029642f76de96b653bf
Jake Jarvis <jake@jarv.is>
2022-01-22 17:16:44 -0500

    fix `<PageTitle />` canonical link

--- components/PageTitle/PageTitle.tsx
+++ components/PageTitle/PageTitle.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import classNames from "classnames";
+import { baseUrl } from "../../lib/config";
 import type { ReactNode } from "react";
 
 import styles from "./PageTitle.module.css";
@@ -12,10 +13,11 @@ type Props = {
 
 const PageTitle = ({ children, className }: Props) => {
   const router = useRouter();
+  const canonical = `${baseUrl}${router.pathname}/`;
 
   return (
     <h1 className={classNames(styles.title, className)}>
-      <Link href={router.asPath}>
+      <Link href={canonical}>
         <a>{children}</a>
       </Link>
     </h1>
