a2921d106eb1e2bb3f635c0c54c9725b8ef7b619
Jake Jarvis <jake@jarv.is>
2022-04-30 09:56:28 -0400

    catch `ThemeScript` minify errors

--- components/ThemeScript/ThemeScript.tsx
+++ components/ThemeScript/ThemeScript.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import { minify } from "uglify-js";
-import { clientScript } from "./script";
+import { clientScript } from "./client";
 import { darkModeQuery, themeStorageKey, themeClassNames } from "../../lib/config/themes";
 
 const ThemeScript = () => {
@@ -12,8 +12,8 @@ const ThemeScript = () => {
       .replace('"__CLASS_NAMES__"', JSON.stringify(themeClassNames));
 
     // minify the final code, a bit hacky but this is ONLY done at build-time, so uglify-js is never bundled or sent to
-    // the browser to execute:
-    return minify(`(${functionString})()`, {
+    // the browser to execute.
+    const result = minify(`(${functionString})()`, {
       toplevel: true,
       compress: {
         negate_iife: false,
@@ -21,15 +21,25 @@ const ThemeScript = () => {
       parse: {
         bare_returns: true,
       },
-    }).code;
+    });
+
+    // fail somewhat silenty
+    if (result.error) {
+      console.error(result.error);
+      return;
+    }
+
+    return result.code;
   }, []);
 
   // the script tag injected manually into `<head>` in _document.tsx.
   // even though it's the proper method, using next/script with `strategy="beforeInteractive"` still causes flash of
   // white on load. injecting a normal script tag lets us prioritize setting the `<html>` class even more urgently.
+  // TODO: using next/script *might* be possible after https://github.com/vercel/next.js/pull/36364 is merged.
   return (
     <script
       key="restore-theme"
+      id="restore-theme"
       dangerouslySetInnerHTML={{
         // make it an IIFE:
         __html: `(function(){${minified}})()`,
