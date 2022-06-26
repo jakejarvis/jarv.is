0ec2bb5bf96ba3915fec2706358bc1138a7d0545
Jake Jarvis <jake@jarv.is>
2022-06-25 22:14:06 -0400

    more fix :)

--- components/HeadingAnchor/HeadingAnchor.tsx
+++ components/HeadingAnchor/HeadingAnchor.tsx
@@ -1,9 +1,9 @@
+import Link from "../Link";
 import { LinkIcon } from "../Icons";
 import { styled } from "../../lib/styles/stitches.config";
 import type { ComponentProps } from "react";
 
-const AnchorLink = styled("a", {
-  textDecoration: "none",
+const AnchorLink = styled(Link, {
   lineHeight: 1,
 });
 
@@ -12,14 +12,14 @@ const Icon = styled(LinkIcon, {
   height: "0.8em",
 });
 
-export type HeadingAnchorProps = ComponentProps<typeof AnchorLink> & {
+export type HeadingAnchorProps = Omit<ComponentProps<typeof AnchorLink>, "href"> & {
   id: string;
   title: string;
 };
 
 const HeadingAnchor = ({ id, title, ...rest }: HeadingAnchorProps) => {
   return (
-    <AnchorLink href={`#${id}`} title={`Jump to "${title}"`} aria-hidden={true} {...rest}>
+    <AnchorLink href={`#${id}`} title={`Jump to "${title}"`} aria-hidden={true} underline={false} {...rest}>
       <Icon />
     </AnchorLink>
   );
