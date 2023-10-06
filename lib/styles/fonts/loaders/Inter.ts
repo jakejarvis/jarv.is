23844c6843b0f33b30ab6a6cb2e92123e8b89bc4
Jake Jarvis <jake@jarv.is>
2023-10-06 10:21:15 -0400

    google fonts workaround

--- lib/styles/fonts/loaders/Inter.ts
+++ lib/styles/fonts/loaders/Inter.ts
@@ -2,6 +2,7 @@ import { Inter as InterLoader } from "next/font/google";
 
 const Inter = InterLoader({
   weight: "variable",
+  axes: ["slnt"], // workaround for lack of italicized Inter on Google Fonts, see: https://github.com/google/fonts/issues/2386#issuecomment-691503098
   subsets: ["latin"],
   display: "fallback",
   preload: true,
