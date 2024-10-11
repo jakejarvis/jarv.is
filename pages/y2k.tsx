f886a7cd0bd4808d2257e67fc998b7a6a3ddbe3d
Jake Jarvis <jake@jarv.is>
2024-10-11 10:48:21 -0400

    bump deps

--- pages/y2k.tsx
+++ pages/y2k.tsx
@@ -12,7 +12,7 @@ import type { ReactElement, ComponentPropsWithoutRef, ElementRef } from "react";
 const VNC = dynamic(() => import("../components/VNC"), { ssr: false });
 
 // https://github.com/jakejarvis/y2k
-const SOCKET_PROXY = "wss://y2k.pipe.fail";
+const SOCKET_PROXY = "wss://vnc.jarv.is";
 
 const Wallpaper = styled("main", {
   display: "flex",
