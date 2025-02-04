87dc68ba8c0b862002159c0b9a142839fed46e2f
Jake Jarvis <jake@jarv.is>
2025-02-04 12:22:20 -0500

    pre-render notes

--- app/notes/[slug]/page.tsx
+++ app/notes/[slug]/page.tsx
@@ -1,14 +1,23 @@
-import Content from "../../../components/Content";
-import PostMeta from "../../../components/PostMeta";
-import Comments from "../../../components/Comments";
-import * as mdxComponents from "../../../lib/helpers/mdx-components";
-import type { PostFrontMatter } from "../../../types";
-
-import { evaluate } from "@mdx-js/mdx";
-import matter from "gray-matter";
 import path from "path";
 import fs from "fs/promises";
 import * as runtime from "react/jsx-runtime";
+import { evaluate } from "@mdx-js/mdx";
+import matter from "gray-matter";
+import Content from "../../../components/Content";
+import PostMeta from "../../../components/PostMeta";
+import Comments from "../../../components/Comments";
+import { getPostSlugs } from "../../../lib/helpers/posts";
+import * as mdxComponents from "../../../lib/helpers/mdx-components";
+import type { PostFrontMatter } from "../../../types";
+
+export async function generateStaticParams() {
+  const slugs = await getPostSlugs();
+
+  // map slugs into a static paths object required by next.js
+  return slugs.map((slug: string) => ({
+    slug,
+  }));
+}
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const fullPath = path.join(process.cwd(), "notes", `${(await params).slug}.mdx`);
