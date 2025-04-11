4cb6ed82ee0f97682708bf6636d9139e92f537d8
Jake Jarvis <jake@jarv.is>
2025-04-11 09:54:16 -0400

    rm instrumentation.ts

--- instrumentation.ts
+++
@@ -1,9 +0,0 @@
-import { registerOTel } from "@vercel/otel";
-
-export const register = () => {
-  if (process.env.VERCEL_ENV === "production") {
-    registerOTel({
-      serviceName: "jarvis",
-    });
-  }
-};
