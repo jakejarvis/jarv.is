f272fb8b5ff7608f57c94c512b163e7418af980a
Jake Jarvis <jake@jarv.is>
2025-05-18 14:53:18 -0400

    improve tweet styling

--- components/third-party/tweet.tsx
+++ components/third-party/tweet.tsx
@@ -14,7 +14,13 @@ const Tweet = async ({ id, className }: { id: string; className?: string }) => {
     const { data } = await fetchTweet(id);
 
     return (
-      <div className={cn("min-h-30 *:mx-auto! *:font-sans!", className)}>
+      <div
+        className={cn(
+          "my-6 min-h-30",
+          "*:[--tweet-body-font-size:var(--text-base)]! *:[--tweet-body-line-height:var(--leading-normal)]! *:[--tweet-container-margin:0_auto]! *:[--tweet-font-family:var(--font-sans)]! *:[--tweet-info-font-size:var(--text-sm)]! *:[--tweet-info-line-height:var(--leading-normal)]!",
+          className
+        )}
+      >
         {data ? (
           <EmbeddedTweet
             tweet={data}
